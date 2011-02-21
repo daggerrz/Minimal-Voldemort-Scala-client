19fd4dc8badcb3990a16aea068465d00ef7cb197
Dag Liodden <dag.liodden@giantleap.no>
2011-02-21 14:30:46 -0500

    Removed scalaj deps. Updated to 2.8.1.

--- project/build/SimpleVoldemortClientProject.scala
+++ project/build/SimpleVoldemortClientProject.scala
@@ -5,18 +5,17 @@ class SimpleVoldemortClientProject(info: ProjectInfo) extends DefaultProject(inf
   override def useDefaultConfigurations = true
 
   // scalaTest
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
   val scalaTest = "org.scalatest" % "scalatest" % "1.2" % "test"
   val mockito = "org.mockito" % "mockito-core" % "1.8.0"
   val junit = "junit" % "junit" % "4.5"
 
   // Voldemort dependencies
-  val scalajCollections = "org.scalaj" %% "scalaj-collection" % "1.0" 
   val googleCollections = "com.google.collections" % "google-collections" % "1.0"
   val commonsLang = "commons-lang" % "commons-lang" % "2.4"
   val jdom = "jdom" % "jdom" % "1.1"
 
   val log4j = "log4j" % "log4j" % "1.2.14"
 
-  override def compileOptions = super.compileOptions ++ Seq(Unchecked, CompileOption("-print"))
+  override def compileOptions = super.compileOptions ++ Seq(Unchecked)
 }
